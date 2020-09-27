d1c91bcb980f59a17b1e7777d6c60f940b0b20c7
Arthur Aleksandro Alves Silva-ALVESSIL <alvessil@motorola.com>
2020-09-27 16:50:59 -0300

    getter and setter Noticia

--- app/src/main/java/br/ufpe/cin/android/rss/Noticia.java
+++ app/src/main/java/br/ufpe/cin/android/rss/Noticia.java
@@ -9,17 +9,58 @@ import java.util.List;
 @Entity(tableName = "noticias")
 public class Noticia {
     @PrimaryKey @NonNull
-    String link;
-    String titulo;
-    String descricao;
-    List<String> categorias;
-    String data;
+    private String link;
+    private String titulo;
+    private String descricao;
+    private List<String> categorias;
+    private String data;
 
     public Noticia(@NonNull String link, String titulo, String descricao, List<String> categorias, String data) {
         this.link = link;
         this.titulo = titulo;
         this.descricao = descricao;
         this.categorias = categorias;
+        this.data = data;
+    }
+
+    @NonNull
+    public String getLink() {
+        return link;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public List<String> getCategorias() {
+        return categorias;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setLink(@NonNull String link) {
+        this.link = link;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setCategorias(List<String> categorias) {
+        this.categorias = categorias;
+    }
+
+    public void setData(String data) {
         this.data = data;
     }
 }
