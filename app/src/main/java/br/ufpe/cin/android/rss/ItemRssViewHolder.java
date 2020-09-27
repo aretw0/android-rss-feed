7aa033ed4357ba8a35e1b2aa3d5221c828c37c55
Arthur Aleksandro Alves Silva-ALVESSIL <alvessil@motorola.com>
2020-09-27 16:58:57 -0300

    melhores comentários e organização

--- app/src/main/java/br/ufpe/cin/android/rss/ItemRssViewHolder.java
+++ app/src/main/java/br/ufpe/cin/android/rss/ItemRssViewHolder.java
@@ -25,22 +25,26 @@ public class ItemRssViewHolder extends RecyclerView.ViewHolder {
 
         // ponto 2 - intent implícita para o browser ao clicar em algum item
         item.setOnClickListener(
-            v -> {
-                // pegando a position
-                int position = getAdapterPosition();
-                RecyclerView conteudoRSS = (RecyclerView) v.getParent();
-                RssAdapter rssAdapter = (RssAdapter) conteudoRSS.getAdapter();
-                //O adapter guarda objetos do tipo Pessoa, de acordo com o parâmetro de tipo.
-                Noticia n = (Noticia) rssAdapter.getItem(position);
-                //Tendo acesso a um objeto noticia, podemos chamar qualquer método disponível.
-
-                // Preparando intent
-                Intent i = new Intent(Intent.ACTION_VIEW);
-                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                i.addCategory(Intent.CATEGORY_BROWSABLE);
-                i.setData(Uri.parse(n.getLink()));
-                v.getContext().startActivity(i);
-            }
+                this::onClick
         );
+    }
+
+    private void onClick(View v) {
+        // Capturando a recyclerView
+        RecyclerView conteudoRSS = (RecyclerView) v.getParent();
+        // Capturando o Adapter
+        RssAdapter rssAdapter = (RssAdapter) conteudoRSS.getAdapter();
+        // Capturando a posição do item no Adapter
+        int position = getAdapterPosition();
+        // O adapter guarda objetos do tipo noticia, posso pegar a noticia da posição equivalente.
+        Noticia n = (Noticia) rssAdapter.getItem(position);
+
+        //Tendo acesso a um objeto noticia, podemos chamar qualquer método disponível.
+        // Preparando intent
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        i.addCategory(Intent.CATEGORY_BROWSABLE);
+        i.setData(Uri.parse(n.getLink()));
+        v.getContext().startActivity(i);
     }
 }
